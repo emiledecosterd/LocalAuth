dbb30ff8035e2624f58c76831e2c224b430867bf
Emile Décosterd <emiledecosterd@mbp-de-emile-2.home>
2016-08-05 19:30:39 +0200

    Initial Commit

---
+++ LocalAuth/LocalAuth.h
@@ -0,0 +1,19 @@
+//
+//  LocalAuth.h
+//  LocalAuth
+//
+//  Created by Emile Décosterd on 05.08.16.
+//  Copyright © 2016 Emile Décosterd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LocalAuth.
+FOUNDATION_EXPORT double LocalAuthVersionNumber;
+
+//! Project version string for LocalAuth.
+FOUNDATION_EXPORT const unsigned char LocalAuthVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LocalAuth/PublicHeader.h>
+
+
