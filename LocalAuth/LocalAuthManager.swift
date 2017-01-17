99eb82e5778abfdbe93ddf81eab19a93b5e1c1d2
Emile Décosterd <emiledecosterd@dynamic.wline.6rd.res.cust.swisscom.ch>
2017-01-17 23:06:32 +0100

    Converted to swift 3

--- LocalAuth/LocalAuthManager.swift
+++ LocalAuth/LocalAuthManager.swift
@@ -33,10 +33,10 @@ import LocalAuthentication
  *  - Unavailable: Chosen policy not available
  *  - Unknown: Unknown error
  */
-public enum LocalAuthError: ErrorType {
-  case System(message:String?)
-  case Unavailable
-  case Unknown
+public enum LocalAuthError: Error {
+  case system(message:String?)
+  case unavailable
+  case unknown
 }
 
 
@@ -52,8 +52,8 @@ public final class LocalAuthManager {
   
   // MARK: Properties
   
-  private var context: LAContext
-  private var localizedReason: String?
+  fileprivate var context: LAContext
+  fileprivate var localizedReason: String?
 
   ///Allows to use this class as a singleton.
   public static var sharedManager = LocalAuthManager()
@@ -103,7 +103,7 @@ public final class LocalAuthManager {
    
    * - Warning: The completion block is executed on the main thread.
    */
-  public func loginUsingTouchID(completion:(Bool)->()) throws {
+  public func loginUsingTouchID(_ completion:@escaping (Bool)->()) throws {
     do{
       try evaluatePolicy(kLAPolicyDeviceOwnerAuthenticationWithBiometrics, completion: completion)
     }catch let error as LocalAuthError {
@@ -120,7 +120,7 @@ public final class LocalAuthManager {
    
    * - Warning: The completion block is executed on the main thread.
    */
-  public func loginUsingSecret(completion: (Bool)->()) throws {
+  public func loginUsingSecret(_ completion: @escaping (Bool)->()) throws {
     do{
       try evaluatePolicy(kLAPolicyDeviceOwnerAuthentication, completion: completion)
     }catch let error as LocalAuthError {
@@ -131,18 +131,18 @@ public final class LocalAuthManager {
   
   // MARK: Private methods
   
-  private func canEvaluatePolicy(policy: LAPolicy) throws{
+  fileprivate func canEvaluatePolicy(_ policy: LAPolicy) throws{
     var error: NSError? = nil
     guard context.canEvaluatePolicy(policy, error: &error) else {
-      throw LocalAuthError.System(message: error?.localizedDescription)
+      throw LocalAuthError.system(message: error?.localizedDescription)
     }
   }
   
   // The completion block will be executed on the main thread.
-  private func evaluatePolicy(policy: Int32, completion: (Bool)->()) throws {
+  fileprivate func evaluatePolicy(_ policy: Int32, completion: @escaping (Bool)->()) throws {
     do{
       guard let policy = LAPolicy(rawValue: Int(policy)) else {
-        throw LocalAuthError.Unknown
+        throw LocalAuthError.unknown
       }
       
       try canEvaluatePolicy(policy)
@@ -153,9 +153,9 @@ public final class LocalAuthManager {
       }
       context.evaluatePolicy(policy, localizedReason: localizedReason!, reply: {(success, evaluateError) -> Void in
         if let error = evaluateError {
-          authError =  LocalAuthError.System(message: error.localizedDescription)
+          authError =  LocalAuthError.system(message: error.localizedDescription)
         }
-        dispatch_async(dispatch_get_main_queue()) {
+        DispatchQueue.main.async {
           completion(success)
         }
       })
